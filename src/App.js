8ec6dfdefd9f9b26dd4a88bb4805b4b7c851a95d
himsoftware@gmail.com <masthimanshu2003@gmail.com>
2018-06-01 16:49:43 +0530

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Bootstrap, Grid, Row, Col, Button} from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div class="container">
+          <div class="card">
+          <article class="card-body">
+          <h4 class="card-title text-center mb-4 mt-1">Sign in</h4><hr></hr>
+            <form>
+                <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+                    </div>
+                    <input v-model="username" name="" class="form-control" placeholder="Email or Username" type="email"/>
+                </div>
+                </div>
+                <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+                    </div>
+                    <input v-model="password" class="form-control" placeholder="******" type="password"/>
+                </div>
+                </div>
+                <div class="form-group">
+                <button type="submit" class="btn btn-primary btn-block"> Login </button>
+                </div>
+              </form>
+          </article>
+          </div>
+        </div>
       </div>
     );
   }
